8d0ab1b2c49c41e6fe6756c6b85fd8d4e9e4c2c4
liuhaiyan05 <liuhaiyan05@baidu.com>
2019-12-04 16:08:28 +0800

    增加了一些注释

--- src/main/java/com/lhy/boot/runner/CommandLineRunnerDemo.java
+++ src/main/java/com/lhy/boot/runner/CommandLineRunnerDemo.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * ApplicationRunner与CommandLineRunner的应用
+ * 这些都是在sprinbbootapplicstion的run方法之后运行
  * ApplicationRunner会先运行
  * @author liuhaiyan
  * @date 2019-12-03 20:48
